e675468d860eae73049667265d7c8dcbd3264b12
Nidhi Bhanushali <71250816+NidhiBhanushali3701@users.noreply.github.com>
2021-04-23 17:59:42 +0530

    updated main.py
    update delete & msgbox added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import sqlite3
 from tkinter.ttk import Treeview
 
@@ -8,57 +9,68 @@ root.configure(background='#F0F8FF')
 root.title('KeyBase')
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
-# Created Tables
-c.execute("""
-CREATE TABLE IF NOT EXISTS users (
-        first_name text, 
-        last_name text, 
-        username text, 
-        password text
-)
-""")
-c.execute("""
-CREATE TABLE IF NOT EXISTS passwords (
-        username text, 
-        application text, 
-        app_email text, 
-        app_username text, 
-        app_password text
-)
-""")
-#commit & close 
+# Created Table
+# c.execute(
+#     """
+#         CREATE TABLE users (
+#             first_name text, last_name text, username text, password text
+#         )
+#     """
+
+
+
+# )
+
+# c.execute(
+#     """
+
+#     CREATE TABLE passwords (
+
+     
+#             username text, application text, app_email text, app_username text, app_password text
+#         )
+#     """
+
+ 
+# )
+
 conn.commit()
 conn.close()
 
 def register_user(first_name, last_name, username, password1, password2):
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
-    global up_register
-    if (first_name!='' and last_name!='' and username!='' and password1!='' and password2!=''):
-      if (password1 == password2):
+
+    c.execute("SELECT username FROM users")
+    username_list = c.fetchall()
+    print("username list: ",username_list)
+    if (username,) in username_list:
+       wrong_username_error = Label(register, text="Username already exists!", fg="red").grid(row=8, column=1, columnspan= 3).grid(row=6, column=1, columnspan= 3)
+
+    elif password1 == '' or password2 == '' or username == '' or first_name == '' or last_name == '':
+        messagebox.showwarning(title='Input Warning', message='Please Enter all Data.')
+
+    elif password1 != password2 :
+        messagebox.showwarning(title='Input Warning', message='Passwords Dont match!!')
+        
+    else:
         password = password1
         c.execute("INSERT INTO users VALUES(:first_name, :last_name, :username, :password)",{'first_name' : first_name, 'last_name':last_name, 'username':username, 'password':password})
-        conn.commit()
-      else:
-        messagebox.showwarning(title='Password Warning', message="Both Password don't match!")
-        print()
-        #wrong_password_error = Label(register, text="Passwords dont match!", fg="red").grid(row=6, column=1, columnspan= 3).grid(row=6, column=1, columnspan= 3)
-      # c.execute("SELECT * FROM users")
-    else:
-      messagebox.showwarning(title='Input Warning', message='Please Enter all Data.')
-      print()
-      #wrong_password_error = Label(register, text="Enter all data!", fg="red").grid(row=6, column=1, columnspan= 3).grid(row=6, column=1, columnspan= 3)
-    
+        message=messagebox.showinfo(title='Successfully Registered!', message='Please proceed to Login.')
+        if message == 'ok':
+            register.destroy()
+        
+       
+    # c.execute("SELECT * FROM users")
+
     first_name_entry.delete(0,END)
     last_name_entry.delete(0,END)
     username_entry.delete(0,END)
     password1_entry.delete(0,END)
     password2_entry.delete(0,END)
 
-    register_success = Label(register, text="Registeration Successful!", fg="green").grid(row=6, column=1, columnspan= 3)
-    c.execute("SELECT * FROM users WHERE username = (:un) AND password = (:pass) ",{'un':username,'pass':password1})
-    up_register = c.fetchall()
-    main_screen(up_register)
+    
+
     conn.commit()
     conn.close()
 
@@ -66,18 +78,17 @@ def register_user(first_name, last_name, username, password1, password2):
 def login_user(username, password):
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
+    global user
     if username!="" and password!="":
         c.execute("SELECT * FROM users WHERE username = ? AND password = ?",(username, password))
-        rows = c.fetchall()
-        if rows:
-            print(rows)
-            main_screen(rows)
+        user = c.fetchall()
+        if user:
+            print(user)
+            main_screen(user)
         else:
-            messagebox.showwarning(title='User Warning', message='No such user exists!')
-            #no_user_error =  Label(login, text="No such user exists!", fg="red").grid(row=4, column=1, columnspan= 3)
+            no_user_error =  Label(login, text="No such user exists!", fg="red").grid(row=4, column=1, columnspan= 3)
     else:
-      messagebox.showerror(title='Input Error', message='Please Enter all Data.')
-      #login_error = Label(login, text="Please ented all fields!", fg="red").grid(row=4, column=1, columnspan= 3)
+        login_error = Label(login, text="Please ented all fields!", fg="red").grid(row=4, column=1, columnspan= 3)
 
     conn.commit()
     conn.close()
@@ -88,19 +99,23 @@ def login():
     login.title("Login Page")
     login.geometry('783x423')
     login.configure(background='#F0F8FF')
+    login.title('Login: KeyBase')
 
     global username_login
     global password_login
-    Label(register, text='Log In as Existing User', bg='#F0F8ff', font=('verdana', 30, 'bold')).place(x=125, y=40)
-    
-    username_login = Entry(login, width=30).place(x=300,y=150,height = 30)
-    password_login = Entry(login, width=30, show="*").place(x=300,y=250,height = 30)
 
-    username_loginlabel = Label(login, text="Username", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=150)
-    password_loginlabel = Label(login, text="Password", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=250)
 
-    login_btn = Button(login, text="Login",bg='#8EE5EE', font=('courier', 15, 'italic'), command=lambda: (login_user(username_login.get(), password_login.get()))).place(x=200,y=350,height=60,width = 175)
-    print(username_login.get(),password_login.get())
+    Label(login, text='Login ', bg='#F0F8FF', font=('verdana', 30, 'bold')).grid(row =0, column=0)
+    username_login = Entry(login, width=30)
+    username_login.grid(row = 2, column=1)
+    password_login = Entry(login, width=30, show="*")
+    password_login.grid(row = 3, column=1)
+
+
+    username_loginlabel = Label(login, text="Username", bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row = 2, column=0)
+    password_loginlabel = Label(login, text="Password", bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row = 3, column=0)
+
+    login_btn = Button(login, text="Login", command=lambda: login_user(username_login.get(), password_login.get())).grid(row = 5, column=0, columnspan=2)
 
 
 def register():
@@ -110,7 +125,7 @@ def register():
     register.configure(background='#F0F8FF')
     register.title('Register: KeyBase')
     
-    Label(register, text='Register as New User', bg='#F0F8ff', font=('verdana', 30, 'bold')).place(x=125, y=40)
+    Label(register, text='Register as New User', bg='#F0F8FF', font=('verdana', 30, 'bold')).grid(row =0, column=0)
 
     global first_name_entry
     global last_name_entry
@@ -119,24 +134,23 @@ def register():
     global password2_entry
     
     first_name_entry = Entry(register, width=30)
-    first_name_entry.place(x=300,y=150,height = 30)
+    first_name_entry.grid(row =2, column = 1)
     last_name_entry = Entry(register, width=30)
-    last_name_entry.place(x=300,y=200,height = 30)
+    last_name_entry.grid(row = 3, column=1)
     username_entry = Entry(register, width=30)
-    username_entry.place(x=300,y=250,height = 30)
+    username_entry.grid(row = 4, column=1)
     password1_entry = Entry(register, width=30)
-    password1_entry.place(x=300,y=300,height = 30)
+    password1_entry.grid(row = 5, column=1)
     password2_entry = Entry(register, width=30)
-    password2_entry.place(x=350,y=355,height = 30)
+    password2_entry.grid(row = 6, column=1)
 
-    first_name_label = Label(register, text='First Name: ', bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=150)
-    last_name_label = Label(register, text="Last Name: ", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=200)
-    username_label = Label(register, text="Username", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=250)
-    password1_label = Label(register, text="Password", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=300)
-    password2_label = Label(register, text="Confirm Password", bg='#F0F8FF', font=('arial', 20, 'normal')).place(x=60,y=350)
+    first_name_label = Label(register, text='First Name: ', bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row= 2, column = 0)
+    last_name_label = Label(register, text="Last Name: ", bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row= 3, column = 0)
+    username_label = Label(register, text="Username", bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row = 4, column=0)
+    password1_label = Label(register, text="Password", bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row = 5, column=0)
+    password2_label = Label(register, text="Confirm Password",  bg='#F0F8FF', font=('arial', 20, 'normal')).grid(row = 6, column=0)
 
-    register_btn = Button(register, text="Register",bg='#8EE5EE', font=('courier', 15, 'italic'),command=lambda: register_user(first_name_entry.get(), last_name_entry.get(), username_entry.get(), password1_entry.get(), password2_entry.get())).place(x=200,y=400,height=60,width = 175)
-    #main_screen(up_register)
+    register_btn = Button(register, text="Register", command=lambda: register_user(first_name_entry.get(), last_name_entry.get(), username_entry.get(), password1_entry.get(), password2_entry.get())).grid(row = 7, column=0, columnspan=2)
 
 
 
@@ -200,14 +214,17 @@ def add_new_password(user):
     app_password_label.grid_remove()
 
     add_new_button.grid_remove()
-
+    
     current_user = user[0][2]
 
     c.execute("SELECT * FROM passwords WHERE username = ?",(current_user,))
     pass_records = c.fetchall()
-    for j in range(1,5):
+    # for j in range(1,5):
 
-        Label(keybase, text=pass_records[len(pass_records)-1][j]).grid(row = (len(pass_records)-1+8), column=(j-1))
+    #     Label(keybase, text=pass_records[len(pass_records)-1][j]).grid(row = (len(pass_records)-1+8), column=(j-1))
+    listbox.insert("","end",values = (len(pass_records),newpass_list[0], newpass_list[1], newpass_list[2], newpass_list[3]))
+    listbox.grid(row = 1, column=0,columnspan = 2)
+
 
     print(pass_records)
     
@@ -217,7 +234,7 @@ def add_new_password(user):
     conn.commit()
     conn.close()
 
-def selected_item(current_user,a):
+def selected_item(a):
     global curItem
     global curr_values_list
     global curItem_id
@@ -226,11 +243,13 @@ def selected_item(current_user,a):
     print(listbox.selection()[0])
     curr_values_list = listbox.item(curItem)['values']
     print(listbox.item(curItem)['values'])
-    
-    update_entry_button = Button(keybase, text="Update an Entry", command=lambda: update_entry_form(current_user,curr_values_list)).grid(row=0,column=2)
+    update_entry_button = Button(keybase, text="Update an Entry", command=update_entry_form).grid(row=0,column=2)
     delete_entry_button = Button(keybase, text="Delete an Entry", command=delete_entry).grid(row=0,column=3)
 
-# delete starts
+
+
+# DELETE START
+
 def delete_entry():
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
@@ -238,52 +257,56 @@ def delete_entry():
     
     c.execute("DELETE FROM passwords WHERE application = ? AND app_username = ?",(curr_values_list[1],curr_values_list[3],))
     listbox.delete(curItem_id)
-    # main_screen(user)
+    
 
 
     conn.commit()
     conn.close()
-        
+
+
 # Update Function form
-def update_entry_form(username,*curr_values_list):
+# Update Function form
+def update_entry_form():
 
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
 
-    global app_name
-    global app_email
-    global app_username
-    global app_password
-    app_name = Entry(keybase,textvariable=curr_values_list[0], width=30,state = DISABLED)
-    app_name.grid(row=2, column=1)
-    #app_name.insert(0,curr_values_list[0])
-    app_email = Entry(keybase, width=30)
-    app_email.grid(row=3, column=1)
-    app_email.insert(0,curr_values_list[1])
-    app_username = Entry(keybase, width=30)
-    app_username.grid(row=4, column=1)
-    app_username.insert(0,curr_values_list[2])
-    app_password = Entry(keybase, width=30)
-    app_password.grid(row=5, column=1)
-    app_password.insert(0,curr_values_list[3])
+    global update_app_name
+    global update_app_email
+    global update_app_username
+    global update_app_password
+    update_app_name = Entry(keybase,textvariable=curr_values_list[0], width=30, state = DISABLED)
+    update_app_name.grid(row=2, column=1)
+    update_app_name.insert(0,curr_values_list[1])
+    update_app_email = Entry(keybase, width=30)
+    update_app_email.grid(row=3, column=1)
+    update_app_email.insert(0,curr_values_list[2])
+    update_app_username = Entry(keybase, width=30)
+    update_app_username.grid(row=4, column=1)
+    update_app_username.insert(0,curr_values_list[3])
+    update_app_password = Entry(keybase, width=30)
+    update_app_password.grid(row=5, column=1)
+    update_app_password.insert(0,curr_values_list[4])
 
     
-    global app_name_label
-    global app_email_label
-    global app_username_label
-    global app_password_label
-    global add_new_button
+    global update_app_name_label
+    global update_app_email_label
+    global update_app_username_label
+    global update_app_password_label
+    global update_password_button
 
-    app_name_label = Label(keybase, text="Application Name ")
-    app_name_label.grid(row=2, column=0)
-    app_email_label = Label(keybase, text="Application email ")
-    app_email_label.grid(row=3, column=0)
-    app_username_label = Label(keybase, text="Application username ")
-    app_username_label.grid(row=4, column=0)
-    app_password_label = Label(keybase, text="Application password ")
-    app_password_label.grid(row=5, column=0)
-    add_new_button = Button(keybase, text="Add", command=lambda: update_user_appDetails(username,curr_values_list[0],app_email.get(),app_username.get(),app_password.get()))#add_new_password(user))
-    add_new_button.grid(row=6,column=0)
+    update_app_name_label = Label(keybase, text="Application Name ")
+    update_app_name_label.grid(row=2, column=0)
+    update_app_email_label = Label(keybase, text="Application email ")
+    update_app_email_label.grid(row=3, column=0)
+    update_app_username_label = Label(keybase, text="Application username ")
+    update_app_username_label.grid(row=4, column=0)
+    update_app_password_label = Label(keybase, text="Application password ")
+    update_app_password_label.grid(row=5, column=0)
+    print("Update function : ",[current_user,curr_values_list[0],curr_values_list[1],update_app_email.get(),update_app_username.get(),update_app_password.get()])
+    update_password_button = Button(keybase, text="Add", command=lambda: update_user_appDetails([current_user,curr_values_list[0],curr_values_list[1],update_app_email.get(),update_app_username.get(),update_app_password.get()]))#add_new_password(user))
+    
+    update_password_button.grid(row=6,column=0)
 
     conn.commit()
     conn.close()
@@ -297,16 +320,11 @@ def update_user_appDetails(user_updated_appDetails):
   app_email = (:app_email) , 
   app_username = (:app_username) , app_password = (:app_password) 
   WHERE username = (:username) AND application = (:app_name)
-  """,{'app_email':user_updated_appDetails[2],'username':user_updated_appDetails[0],'app_name':user_updated_appDetails[1],
-  'app_username':user_updated_appDetails[3],'app_password':user_updated_appDetails[4]})
+  """,{'app_email':user_updated_appDetails[3],'username':user_updated_appDetails[0],'app_name':user_updated_appDetails[2],
+  'app_username':user_updated_appDetails[4],'app_password':user_updated_appDetails[5]})
+  listbox.item(curItem, text="", values=(user_updated_appDetails[1],user_updated_appDetails[2], user_updated_appDetails[3], user_updated_appDetails[4], user_updated_appDetails[5]))
   conn.commit()
-  conm.close()
-
-
-    
-
-
-
+  conn.close()
 
 
 
@@ -322,6 +340,7 @@ def main_screen(user):
     
 
     add_password_button = Button(keybase, text="Add new Password", command=lambda: add_password_form(user)).grid(row=0,column=0)
+    global current_user
     current_user = user[0][2]
     print(current_user)
     c.execute("SELECT * FROM passwords WHERE username = ?",(current_user,))
@@ -350,8 +369,8 @@ def main_screen(user):
     global listbox
     label = Label(keybase, text="Your Passwords", font=("Arial",30)).grid(row=0, columnspan=3)
     cols = ('Serial Number','Application Name','Email','Username','Password')
-    listbox = Treeview(keybase, columns = cols, show = 'headings')
-    listbox.bind('<ButtonRelease-1>', selected_item(current_user))
+    listbox = Treeview(keybase, columns = cols, show = 'headings', selectmode='browse')
+    listbox.bind('<ButtonRelease-1>', selected_item)
     for i, (appname, appmail, appusername, apppassword) in enumerate(templist, start = 1):
         listbox.insert("","end",values = (i,appname, appmail, appusername, apppassword))
 
@@ -374,8 +393,10 @@ def main_screen(user):
 
 
 Label(root, text='Welcome to Keybase!', bg='#F0F8FF', font=('verdana', 30, 'bold')).place(x=143, y=86)
-login = Button(root, text='Login', bg='#8EE5EE', font=('courier', 15, 'italic'), command=login).place(x=233, y=186,height = 50)
-register = Button(root, text='Register',bg='#8EE5EE', font=('courier', 15, 'italic'), command=register).place(x=429, y=186,height = 50)
+login = Button(root, text='Login', bg='#8EE5EE', font=('courier', 10, 'italic'), command=login)
+login.place(x=233, y=186)
+register = Button(root, text='Register', bg='#8EE5EE', font=('courier', 10, 'italic'), command=register)
+register.place(x=429, y=186)
 
 
 
